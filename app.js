ab839ac8ca429a02acfe0170cb769e66eb21cc23
Yi Kai <beliefs111@gmail.com>
2016-09-11 10:32:19 +0800

    only log errors

--- app.js
+++ app.js
@@ -15,7 +15,9 @@ app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(logger('combined', {
+  skip: function (req, res) { return res.statusCode < 400 }
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
